3681d841a8dd57328daa28321fb3b5db63a59c2c
semodi <sebastian-dick@gmx.net>
2019-07-30 10:36:04 -0400

    integer no longer attached to species labels

--- siesta_engine/calculator.py
+++ siesta_engine/calculator.py
@@ -2,6 +2,10 @@ from ase.calculators.siesta.siesta import Siesta
 from ase.calculators.calculator import Calculator, all_changes
 import shutil
 import os
+# from ase.calculators.siesta.siesta import *
+from ase.calculators.siesta.base_siesta import *
+# from ase.calculators.siesta import *
+
 if not 'SIESTA_COMMAND' in os.environ:
     os.environ['SIESTA_COMMAND'] = 'siesta < ./%s > ./%s'
 
@@ -12,6 +16,8 @@ class CustomSiesta(Siesta):
         if 'label' in kwargs:
             kwargs['label'] = kwargs['label'].lower()
         super().__init__(*args, **kwargs)
+        self.parameters.update({'symlink_pseudos': None})
+
 
 
     def write_input(self, atoms, properties=None, system_changes=None):
@@ -50,6 +56,149 @@ class CustomSiesta(Siesta):
                 with open(filename, 'w') as ase_fdf:
                     ase_fdf.write(tmp_file.read())
 
+    def _write_species(self, f, atoms):
+        """Write input related the different species.
+
+        Parameters:
+            - f:     An open file object.
+            - atoms: An atoms object.
+        """
+        species, species_numbers = self.species(atoms)
+
+        if not self['pseudo_path'] is None:
+            pseudo_path = self['pseudo_path']
+        elif 'SIESTA_PP_PATH' in os.environ:
+            pseudo_path = os.environ['SIESTA_PP_PATH']
+        else:
+            mess = "Please set the environment variable 'SIESTA_PP_PATH'"
+            raise Exception(mess)
+
+        f.write(format_fdf('NumberOfSpecies', len(species)))
+        f.write(format_fdf('NumberOfAtoms', len(atoms)))
+
+        pao_basis = []
+        chemical_labels = []
+        basis_sizes = []
+        synth_blocks = []
+        for species_number, spec in enumerate(species):
+            species_number += 1
+            symbol = spec['symbol']
+            atomic_number = atomic_numbers[symbol]
+
+            if spec['pseudopotential'] is None:
+                    label = symbol
+                    pseudopotential = label + '.psf'
+            else:
+                pseudopotential = spec['pseudopotential']
+                label = os.path.basename(pseudopotential)
+                label = '.'.join(label.split('.')[:-1])
+
+            if not os.path.isabs(pseudopotential):
+                pseudopotential = join(pseudo_path, pseudopotential)
+
+            if not os.path.exists(pseudopotential):
+                mess = "Pseudopotential '%s' not found" % pseudopotential
+                raise RuntimeError(mess)
+
+            name = os.path.basename(pseudopotential)
+            name = name.split('.')
+            if spec['ghost']:
+                name.insert(-1, 'ghost')
+                atomic_number = -atomic_number
+
+            name = '.'.join(name)
+            pseudo_targetpath = self.getpath(name)
+
+            if join(os.getcwd(), name) != pseudopotential:
+                if islink(pseudo_targetpath) or isfile(pseudo_targetpath):
+                    os.remove(pseudo_targetpath)
+                symlink_pseudos = self['symlink_pseudos']
+
+                if symlink_pseudos is None:
+                    symlink_pseudos = not os.name == 'nt'
+
+                if symlink_pseudos:
+                    os.symlink(pseudopotential, pseudo_targetpath)
+                else:
+                    shutil.copy(pseudopotential, pseudo_targetpath)
+
+            if not spec['excess_charge'] is None:
+                atomic_number += 200
+                n_atoms = sum(np.array(species_numbers) == species_number)
+
+                paec = float(spec['excess_charge']) / n_atoms
+                vc = get_valence_charge(pseudopotential)
+                fraction = float(vc + paec) / vc
+                pseudo_head = name[:-4]
+                fractional_command = os.environ['SIESTA_UTIL_FRACTIONAL']
+                cmd = '%s %s %.7f' % (fractional_command,
+                                      pseudo_head,
+                                      fraction)
+                os.system(cmd)
+
+                pseudo_head += '-Fraction-%.5f' % fraction
+                synth_pseudo = pseudo_head + '.psf'
+                synth_block_filename = pseudo_head + '.synth'
+                os.remove(name)
+                shutil.copyfile(synth_pseudo, name)
+                synth_block = read_vca_synth_block(
+                    synth_block_filename,
+                    species_number=species_number)
+                synth_blocks.append(synth_block)
+
+            if len(synth_blocks) > 0:
+                f.write(format_fdf('SyntheticAtoms', list(synth_blocks)))
+
+            label = '.'.join(np.array(name.split('.'))[:-1])
+            string = '    %d %d %s' % (species_number, atomic_number, label)
+            chemical_labels.append(string)
+            if isinstance(spec['basis_set'], PAOBasisBlock):
+                pao_basis.append(spec['basis_set'].script(label))
+            else:
+                basis_sizes.append(("    " + label, spec['basis_set']))
+        f.write((format_fdf('ChemicalSpecieslabel', chemical_labels)))
+        f.write('\n')
+        f.write((format_fdf('PAO.Basis', pao_basis)))
+        f.write((format_fdf('PAO.BasisSizes', basis_sizes)))
+        f.write('\n')
+
+
+    def read_ion(self, atoms):
+        """Read the ion.xml file of each specie
+        """
+        from ase.calculators.siesta.import_ion_xml import get_ion
+
+        species, species_numbers = self.species(atoms)
+
+        self.results['ion'] = {}
+        for species_number, spec in enumerate(species):
+            species_number += 1
+
+            symbol = spec['symbol']
+            atomic_number = atomic_numbers[symbol]
+
+            if spec['pseudopotential'] is None:
+                label = symbol
+                pseudopotential = self.getpath(label, 'psf')
+            else:
+                pseudopotential = spec['pseudopotential']
+                label = os.path.basename(pseudopotential)
+                label = '.'.join(label.split('.')[:-1])
+
+            name = os.path.basename(pseudopotential)
+            name = name.split('.')
+            if spec['ghost']:
+                name.insert(-1, 'ghost')
+                atomic_number = -atomic_number
+            name = '.'.join(name)
+
+            label = '.'.join(np.array(name.split('.'))[:-1])
+
+            if label not in self.results['ion']:
+                fname = self.getpath(label, 'ion.xml')
+                if os.path.isfile(fname):
+                    self.results['ion'][label] = get_ion(fname)
+
     def calculate(self, atoms=None, properties=['energy'],
                   system_changes=all_changes):
 
@@ -71,6 +220,14 @@ class CustomSiesta(Siesta):
             self.read_results()
         else:
             super().calculate(atoms, properties, system_changes)
+
+    def getpath(self, fname=None, ext=None):
+        """ Returns the directory/fname string """
+        if fname is None:
+            fname = self.prefix
+        if ext is not None:
+            fname = '{}.{}'.format(fname, ext)
+        return os.path.join(self.directory, fname)
 
 
 
